1feb143ffb9a11d73ff213dd484c0f0967318ebb
Dmitry Suhoversky <dmitrysuhov@outlook.com>
2021-01-09 15:12:09 +0300

    Initial Commit

---
+++ Reality/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Reality
+//
+//  Created by Dmitry Suhoversky on 9.01.21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
